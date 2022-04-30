01217459abfb59f4399f6aec5999c66eec0f606c
Your Name <you@example.com>
2022-04-30 13:48:44 -0500

    plotting code for latencies

---
+++ analyze_results.py
@@ -0,0 +1,34 @@
+import sys
+import joblib
+import matplotlib.pyplot as plt
+import numpy as np
+
+if __name__ == "__main__":
+    if (len(sys.argv) != 2):
+        print("Need arguments: results file name, config file name.")
+    
+    res_fname = sys.argv[1]
+    
+    res = joblib.load(res_fname)
+
+    # TODO: Load this from file
+    roundsPerObservationPeriod = 10
+    numConsensusRounds = 100
+    
+    numObservationPeriods = numConsensusRounds // roundsPerObservationPeriod
+
+    # for i, round_res in enumerate(res.perRoundResults):
+    #     print('Round', i, round_res.latenciesByNode)
+    
+    # consensuses_by_node = []
+    round_latencies = [round_res.latenciesByNode for round_res in res.perRoundResults]
+    max_round_latencies = [min([v for k, v in lats[1].items()]) for lats in round_latencies]
+
+    obs_period_lats = [np.average(max_round_latencies[i*roundsPerObservationPeriod:(i+1)*roundsPerObservationPeriod]) for i in range(numObservationPeriods)]
+
+    plt.plot(obs_period_lats, '-x')
+    plt.title('Observation Period Latencies')
+    plt.xlabel('Observation Period')
+    plt.ylabel('Latency (ms)')
+    
+    plt.show()

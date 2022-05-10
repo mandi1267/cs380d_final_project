2ea9fb8ba64c4d39c7c829cf668a49398b08bd43
definitelyuncertain <saikirann94@gmail.com>
2022-05-10 14:18:10 -0500

    reset writer to n=10

--- byzantine_mab_config_writer.py
+++ byzantine_mab_config_writer.py
@@ -38,8 +38,8 @@ def createConfigs():
     # maxFaulty = 21  # (numNodes-1)/3
     # numNodes = 34
     # maxFaulty = 11
-    numNodes = 13
-    maxFaulty = 4
+    numNodes = 10
+    maxFaulty = 3
 
 
     # possibleMValues = [1, 6, 11, 16, 21]  # TODO is this good?
@@ -47,7 +47,7 @@ def createConfigs():
     useCentralizedMultiArmedBandit = True
     # possibleMValues = [1, 4, 6, 9, 11]  # TODO is this good?
     # possibleMValues = [1, 2, 3, 4, 5]  # TODO is this good?
-    possibleMValues = [0, 1, 2, 3, 4]
+    possibleMValues = [0, 1, 2, 3]
 
     # TODO these are kind of arbitrary, but don't reaaaally affect the results, so that's okay
     averageLatencyMs = 20

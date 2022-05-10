505b416efc5b44c4b0e80737d299f58d93b485ae
definitelyuncertain <saikirann94@gmail.com>
2022-05-09 23:47:48 -0500

    tested bandit with om algo, using didfail to assign reward

--- multiarmed_bandit_executor.py
+++ multiarmed_bandit_executor.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class MultiArmedBanditExecutor:
     """
     Object that should take in results information and determine the value(s) of m to use for the next observation
@@ -13,18 +15,69 @@ class MultiArmedBanditExecutor:
         """
         self.mOptions = mOptions
         self.multiArmedBanditConfig = multiArmedBanditConfig
+        self.n_arms = len(self.mOptions)
+        self.ni = np.zeros(self.n_arms)
+        self.si = np.zeros(self.n_arms)
+        self.prev_l = None
+        self.latency_scale = 1e-3
+        self.i = 0
+        self.gamma = 0.5
+        self.lat_rew_bias = 1.0
+        self.failure_penalty = -0.5
         # TODO
 
     def getNextValueOfM(self, resultsSinceLastRound):
         """
         Get the next value of m to use.
 
-        :param resultsSinceLastRound:   Results (List of SingleRoundResults obj) since the last time an m value was chosen.
+        :param resultsSinceLastRound:   Results (list of SingleRoundResults obj) since the last time an m value was chosen.
 
         :return: Next m value to use.
         """
-        # TODO
-        return self.mOptions[0]
+        # Get the observations from the previous arm pull and which arm it was
+        round_latencies = [round_res.latenciesByNode for round_res in resultsSinceLastRound]
+        avg_latency = np.average([max([v for k, v in (list(lats.items())[0][1]).items()]) for lats in round_latencies])
+        print('Prev_l', self.prev_l, 'Latencies', avg_latency)
+        # round_consensuses = [round_res.consensusesByNode for round_res in resultsSinceLastRound]
+        # round_failures = np.where([np.average([v for k, v in (list(cons.items())[0][1]).items()])<=0.5 for cons in round_consensuses])
+        round_didFails = [round_res.didFail for round_res in resultsSinceLastRound]
+        round_failures = np.where([(list(f.items())[0][1]) for f in round_didFails])
+        # print(round_failures)
+
+        # for round_res in resultsSinceLastRound:
+            # print('round failures', [([v for k, v in (list(cons.items())[0][1]).items()]) for cons in round_consensuses])
+            # print('failures', round_failures[0].size)
+
+        if (self.prev_l is not None):
+            # Update arm pull and reward counters
+            self.ni *= self.gamma # Discounting
+            self.si *= self.gamma # Discounting
+            self.ni[self.prev_l] += 1
+            # TODO: change how reward is calculated to include failure penalties etc.
+            rew = 0.0
+            if round_failures[0].size == 0:
+                rew = (self.lat_rew_bias - avg_latency*self.latency_scale)
+            else:
+                rew = self.failure_penalty
+            self.si[self.prev_l] += rew
+
+        if (self.prev_l is None) or (np.any(self.ni==0)):
+            # Initial rounds, make sure every arm is pulled at least once
+            l = np.max(np.where(self.ni==0)[0])
+        else:
+            # Regular decisions after initial rounds
+            mui = self.si/self.ni
+            nt = np.sum(self.ni)
+            ucb = np.maximum(mui*(1-mui), 0.002)*np.log(nt)/self.ni
+            ucb = np.sqrt(ucb)
+            l = np.argmax(mui + ucb)
+        
+        self.prev_l = l
+
+        print(f'ni {self.ni}\n si {self.si}')
+        print(f'Decision: {l}, m={self.mOptions[l]}\n============')
+        return self.mOptions[l]
+        # return self.mOptions[1]
 
     def getNextValuesOfM(self, resultsSinceLastRound, minMValueMargin):
         """
